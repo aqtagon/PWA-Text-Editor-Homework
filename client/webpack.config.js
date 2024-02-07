b174ca3cc070ebd69c2e5084cd175ed7a0f4d97d
aqtagon <ahmed.danawi@gmail.com>
2024-02-06 20:49:54 -0800

    Added to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,70 +1,72 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'JATE',
-      }),
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'service-worker.js',
-      }),
-      new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: 'Just Another Text Editor',
-        short_name: 'JATE',
-        description: 'A text editor that works offline',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
-        icons: [
-          {
-            src: path.resolve('src', 'images', 'icon.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
-    ],
-
-    module: {
-      rules: [
+module.exports = {
+  mode: 'production',
+  // Entry point for your application
+  entry: './src/index.js',
+  // Output configuration for Webpack
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    // Generates an HTML file from a template
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    // Generates a manifest file for your PWA
+    new WebpackPwaManifest({
+      name: 'My Progressive Web App',
+      short_name: 'MyPWA',
+      description: 'My awesome Progressive Web App!',
+      background_color: '#ffffff',
+      crossorigin: 'use-credentials', // Can be null, use-credentials or anonymous
+      inject: true,
+      fingerprints: false,
+      icons: [
         {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+          destination: path.join('assets', 'icons'),
         },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-            },
-          },
-        },
-        
       ],
-    },
-  };
+    }),
+    // Workbox plugin to generate a service worker
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'service-worker.js',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // JavaScript files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // Transpile ES6+ to ES5
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/, // CSS files
+        use: ['style-loader', 'css-loader'], // Inject CSS into the DOM
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i, // Image files
+        type: 'asset/resource',
+      },
+    ],
+  },
+  // Development server configuration
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
 };

91adf5d909f1b713d064ff2ac81b554a537b15b2
aqtagon <ahmed.danawi@gmail.com>
2024-02-06 18:50:00 -0800

    Added more to pluins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,6 +26,24 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'service-worker.js',
       }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A text editor that works offline',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src', 'images', 'icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
